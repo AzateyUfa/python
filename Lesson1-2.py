81cca91ccd334798aebef96f44e8d7ef4b645503
AzateyUfa <azat@maksutov.org>
2021-05-18 10:41:17 +0500

    Initial commit

---
+++ Lesson1-2.py
@@ -0,0 +1,7 @@
+# Задание 2
+time=int(input("Введите время в секундах: "))
+hours=time // 3600
+minutes=(time - hours * 3600) // 60
+seconds=time - (hours * 3600 + minutes * 60)
+print(f"чч:мм:сс {hours} : {minutes} : {seconds}")
+print("end")
